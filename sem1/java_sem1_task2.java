d53231bab1ae59b86358e614cb52287c5196404a
kira.dob <arik.dobrynina@yandex.ru>
2023-03-27 11:33:33 +0700

    added task 1

--- sem1/java_sem1_task2.java
+++ sem1/java_sem1_task2.java
@@ -1,15 +1,20 @@
 package sem1;
+
+import java.util.Arrays;
+
 // Дан массив nums = [3,2,2,3] и число val = 3. Если в массиве есть числа, равные заданному, 
 // нужно перенести эти элементы в конец массива. Таким образом, первые несколько (или все) элементов 
 // массива должны быть отличны от заданного, а остальные - равны ему.
 public class java_sem1_task2 {
     public static void main(String[] args) {
         int[] array={3,2,2,2,1,5,3,2,3};
+        System.out.println(Arrays.toString(array));
         int val = 3;
         swap(array,val);
-        for (int x:array){
-            System.out.print(x+" ");
-        }
+        System.out.println(Arrays.toString(array));
+        // for (int x:array){
+        //     System.out.print(x+" ");
+        // }
     }
 
         // Метод, который принимает массив и число и в массиве делает операцию
